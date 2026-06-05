1ca5507faad40b57b73b88f83e30e5e991abcc8e
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-05 10:38:15 +0200

    a Add task

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_stats(total_size, status_codes):
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+def main():
+    total_size = 0
+    status_codes = {
+        "200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, '404': 0, "404": 0, "500": 0
+    }
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            tokens = line.split()
+            file_size = tokens[-1]
+            total_size += int(file_size)
+            status_code = tokens[-2]
+
+            if status_code in status_code:
+                status_codes[status_code] += 1
+
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
